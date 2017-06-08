2545a4fbcde135dae7632726ebb08a9e7c282b2c
roboticaholycross <tomas@p4d.com.ar>
2017-06-08 08:45:23 -0300

    +node install instructions

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # rpiarduino-serialcom
+
+
+
+# instalar node
+sudo apt-get remove nodered -q -y --force-yes
+sudo apt-get remove nodejs nodejs-legacy -q -y --force-yes
+curl -sL https://deb.nodesource.com/setup_7.x | sudo -E bash -
--- readFromArduino.js
+++ readFromArduino.js
@@ -3,12 +3,12 @@ var shell = require('shell')
 
 var port = new SerialPort('/dev/cu.usbmodem1411', { baudRate: 9600 })
 
-port.on('data', function(info) {
-  if(info == 4) {
+port.on('data', function (info) {
+  if (info == 4) {
     shell.run('4.py')
   }
 
-  if(info == 6) {
+  if (info == 6) {
     shell.run('6.py')
   }
 })
